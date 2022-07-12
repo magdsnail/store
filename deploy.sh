579d86b8f630a3882ff00fe89c587b0a1fa1c961
jason.wang <wangkjim@gmail.com>
2022-07-12 20:22:11 +0800

    ..

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,20 @@ npm run build
 
 cd docs/.vuepress/dist
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy manually'
-  githubUrl=git@github.com:magdsnail/store.git
-else
+echo ${GITHUB_TOKEN}
+echo "========="
+
+# if [ -z "$GITHUB_TOKEN" ]; then
+#   msg='deploy manually'
+#   githubUrl=git@github.com:magdsnail/store.git
+# else
   msg='github actions'
   githubUrl=https://magdsnail:${GITHUB_TOKEN}@github.com/magdsnail/store.git
   git config --global user.name "jason.wang"
   git config --global user.email "wangkjim@gmail.com"
-fi
-git config --global user.name "jason.wang"
-git config --global user.email "wangkjim@gmail.com"
+# fi
+# git config --global user.name "jason.wang"
+# git config --global user.email "wangkjim@gmail.com"
 git init
 git add -A
 git commit -m "${msg}"
