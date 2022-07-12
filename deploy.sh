bed26ddd97d2fa65742664f10660fcf79a6ca636
jason.wang <wangkjim@gmail.com>
2022-07-12 17:57:39 +0800

    ..

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,22 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+if [ -z "$ACCESS_TOKEN" ]; then
+  msg='deploy manually'
+  githubUrl=git@github.com:magdsnail/store.git
+else
+  msg='github actions'
+  githubUrl=https://magdsnail:${ACCESS_TOKEN}@github.com/magdsnail/store.git
+  git config --global user.name "ccbean"
+  git config --global user.email "liuyh940@gmail.com"
+fi
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:magdsnail/store.git master:gh-pages
+git push -f $githubUrl master:gh-pages
 
 cd -
 rm -rf dist
